b58797ef7cbe2baa7cb769e7bb4315252a73df06
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-11-11 15:49:25 -0300

    Add header implementation

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,26 @@
+import Image from 'next/image'
+
+import appConfig from '@/aluratube.config.json'
+
 function Header() {
-    return <div>Header</div>
+    return (
+        <>
+            <Image
+                src={appConfig.user.banner}
+                width={2072}
+                height={1379}
+                alt="A banner image"
+            />
+            <Image
+                src={`https://github.com/${appConfig.user.github}.png`}
+                width={420}
+                height={420}
+                alt="A user photo from github profile"
+            />
+            <h2>{appConfig.user.name}</h2>
+            <h3>{appConfig.user.description}</h3>
+        </>
+    )
 }
 
 export default Header
