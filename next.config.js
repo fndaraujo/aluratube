f5d110205b0160ca05c8e4383dee7532580a0349
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-11-11 15:43:10 -0300

    Add remote server entry for external image access

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
+}
