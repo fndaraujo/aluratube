15bd39121b9be26f1319c283f6d6c9d8a78727b0
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-11-12 18:44:24 -0300

    Add fetch database videos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
+import React from 'react'
+
 import Timeline from '@/components/Timeline'
+import { supabase } from '@/lib/supabase-service'
 
 function HomePage() {
+    const [playlists, setPlaylists] = React.useState({})
+
+    React.useEffect(() => {
+        supabase
+            .from('video')
+            .select('*')
+            .order('playlist')
+            .order('id')
+            .then((videos) => {
+                const _playlists = {}
+                videos.data.forEach((video) => {
+                    if (!_playlists[video.playlist]) {
+                        _playlists[video.playlist] = []
+                    }
+                    _playlists[video.playlist] = [
+                        video,
+                        ..._playlists[video.playlist],
+                    ]
+                })
+                setPlaylists(_playlists)
+            })
+    }, [])
+
     return <Timeline />
 }
 
