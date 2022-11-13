aba34ea738a80d17612e700eda9a7db2183af10c
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-11-13 20:52:03 -0300

    Add timeline listen search bar

--- components/Timeline/index.js
+++ components/Timeline/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-function Timeline(props) {
+function Timeline({ searchValue, ...props }) {
     const playlistNames = Object.keys(props.playlists)
 
     return (
@@ -11,20 +11,24 @@ function Timeline(props) {
                     <section key={playlistName}>
                         <h3>{playlistName}</h3>
                         <div>
-                            {videos.map((video) => {
-                                return (
-                                    <a key={video.url} href={video.url}>
-                                        <Image
-                                            src={video.thumbnail}
-                                            width={1280}
-                                            height={720}
-                                            alt={`${video.title} thumbnail`}
-                                        />
-                                        <span>{video.title}</span>
-                                        <br />
-                                    </a>
-                                )
-                            })}
+                            {videos
+                                .filter((video) => {
+                                    return video.title.includes(searchValue)
+                                })
+                                .map((video) => {
+                                    return (
+                                        <a key={video.url} href={video.url}>
+                                            <Image
+                                                src={video.thumbnail}
+                                                width={1280}
+                                                height={720}
+                                                alt={`${video.title} thumbnail`}
+                                            />
+                                            <span>{video.title}</span>
+                                            <br />
+                                        </a>
+                                    )
+                                })}
                         </div>
                     </section>
                 )
