b541721a3b516a5c03688a6a869177313f406fed
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-11-13 19:15:22 -0300

    Add search bar to menu

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,7 +1,14 @@
+import SearchBar from '@/components/Menu/searchbar'
+
 import appConfig from '@/aluratube.config.json'
 
 function Menu() {
-    return <div>{appConfig.app.product}</div>
+    return (
+        <div>
+            {appConfig.app.product}
+            <SearchBar />
+        </div>
+    )
 }
 
 export default Menu
