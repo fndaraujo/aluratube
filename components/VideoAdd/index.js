59d2b84fc0451c23ac6b7825e908cafe100932e0
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-11-13 02:56:56 -0300

    Add handle input data on video-add form

--- components/VideoAdd/index.js
+++ components/VideoAdd/index.js
@@ -1,11 +1,40 @@
+import React from 'react'
+
+function useForm(props) {
+    const [values, setValues] = React.useState(props.initialValues)
+    return {
+        values,
+        handleChange: (e) => {
+            const value = e.target.value
+            const name = e.target.name
+            setValues({ ...values, [name]: value })
+        },
+    }
+}
+
 function VideoAdd() {
+    const formValues = useForm({ initialValues: { title: '', url: '' } })
     return (
         <div>
             <button>+</button>
-            <form>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                }}
+            >
                 <button>x</button>
-                <input placeholder="Enter video title" />
-                <input placeholder="Enter video url" />
+                <input
+                    placeholder="Enter video title"
+                    name="title"
+                    value={formValues.values.title}
+                    onChange={formValues.handleChange}
+                />
+                <input
+                    placeholder="Enter video url"
+                    name="url"
+                    value={formValues.values.url}
+                    onChange={formValues.handleChange}
+                />
                 <button type="submit">Add</button>
             </form>
         </div>
